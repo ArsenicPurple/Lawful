ec74c01b1c97fcbe4be372380a631ffdd4bb1d7b
ArsenicPurple <56276124+ArsenicPurple@users.noreply.github.com>
2021-11-14 14:51:41 -0500

    Initial Push

---
+++ src/main/java/co/basin/lawfulmod/LawfulMod.java
@@ -0,0 +1,42 @@
+package co.basin.lawfulmod;
+
+import co.basin.lawfulmod.core.init.ItemInit;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.stream.Collectors;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(LawfulMod.MOD_ID)
+public class LawfulMod
+{
+    public static final String MOD_ID = "lawful";
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public LawfulMod() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        bus.addListener(this::setup);
+
+        ItemInit.ITEMS.register(bus);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+
+    }
+}

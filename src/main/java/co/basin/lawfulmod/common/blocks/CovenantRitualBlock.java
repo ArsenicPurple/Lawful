565ab193fa194bb4cede144fef3f4f57b36e6fb5
ArsenicPurple <56276124+ArsenicPurple@users.noreply.github.com>
2021-11-21 13:35:02 -0500

    Covenant Ritual Block NBT Code

--- src/main/java/co/basin/lawfulmod/common/blocks/CovenantRitualBlock.java
+++ src/main/java/co/basin/lawfulmod/common/blocks/CovenantRitualBlock.java
@@ -5,6 +5,8 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.ListNBT;
+import net.minecraft.nbt.LongArrayNBT;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
@@ -13,6 +15,7 @@ import net.minecraft.world.World;
 
 public class CovenantRitualBlock extends Block {
     public static final String TAG_EMITTING_BLOCKS = "emittingBlocks";
+    private BlockPos[] emittingBlocksCache = null;
 
     public CovenantRitualBlock(Properties properties) {
         super(properties);
@@ -27,7 +30,21 @@ public class CovenantRitualBlock extends Block {
         return ActionResultType.CONSUME;
     }
 
+    public void setEmittingBlocks(ItemStack stack, BlockPos[] positions) {
+        long[] array = new long[positions.length];
+        for (int i = 0; i < positions.length; i++) {
+            array[i] = positions[i].asLong();
+        }
+        stack.addTagElement(TAG_EMITTING_BLOCKS, new LongArrayNBT(array));
+    }
+
     public BlockPos[] getEmittingBlocks(ItemStack stack) {
-        ItemNBTUtil.getList(stack, TAG_EMITTING_BLOCKS, BlockPos.class.hashCode(), false);
+        if (emittingBlocksCache != null) { return emittingBlocksCache; }
+        long[] array = stack.getOrCreateTag().getLongArray(TAG_EMITTING_BLOCKS);
+        BlockPos[] positions = new BlockPos[array.length];
+        for (int i = 0; i < array.length; i++) {
+            positions[i] = BlockPos.of(array[i]);
+        }
+        return (emittingBlocksCache = positions);
     }
 }
